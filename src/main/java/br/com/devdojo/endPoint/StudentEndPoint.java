903490c594b0eaff3500649a03f1068655e1c821
eduardomunareto <eduardomunareto@icloud.com>
2021-09-17 17:55:11 -0300

    aula 8 concluída

--- src/main/java/br/com/devdojo/endPoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endPoint/StudentEndPoint.java
@@ -6,10 +6,7 @@ import br.com.devdojo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,14 +29,12 @@ private final DateUtil dateUtil;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-
     public ResponseEntity<?> listAll() {
         System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
-
     public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
         Student student = new Student();
         student.setId(id);
@@ -47,5 +42,11 @@ private final DateUtil dateUtil;
         if(index == -1)
             return new ResponseEntity<Object>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+    }
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<Object>(student, HttpStatus.OK);
+
     }
 }
