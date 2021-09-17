c61b866833a25567a9878fb8ea5055e21ebf7f9e
eduardomunareto <eduardomunareto@icloud.com>
2021-09-17 10:02:29 -0300

    Primeira parte de testes do programa, video 04

---
+++ src/test/java/br/com/devdojo/demo3/SpringBootEssentialsTests.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.demo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
