3a75f4b7d8774602779d65be2d1166cd79b20718
eduardomunareto <eduardomunareto@icloud.com>
2021-09-19 17:21:36 -0300

    Terminei aula 16

--- src/main/java/br/com/devdojo/error/ResourceNotFoundDetails.java
+++ src/main/java/br/com/devdojo/error/ResourceNotFoundDetails.java
@@ -1,6 +1,6 @@
 package br.com.devdojo.error;
 
-public class ResourceNotFoundDetails extends ErrorDetail {
+public class ResourceNotFoundDetails extends ErrorDetails {
 
     private String title;
     private int status;
